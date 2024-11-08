5176d13bdd31005bd1f5e727df67ae99ac33f91d
creamrain <leerennie17@gmail.com>
2024-11-08 17:35:11 +0800

    Doing dip for Medical Record management

---
+++ HealthManagementSystem/src/MedicalRecord/PatientMedicalRecordManager.java
@@ -0,0 +1,19 @@
+package MedicalRecord;
+
+public class PatientMedicalRecordManager implements MedicalRecordService{
+
+    
+    public PatientMedicalRecordManager(){
+
+    }
+    
+    public void viewMedicalRecord(String patientID){
+
+
+    }
+
+    public void updateInformation(String patientID, String newEmailAddress, String newContactNumber){
+
+    }
+
+}
