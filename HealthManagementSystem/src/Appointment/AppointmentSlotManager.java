7692047d0d29c6d0d20811b5c1cb3c08c4a3ec43
creamrain <leerennie17@gmail.com>
2024-11-14 21:08:39 +0800

    Minor adjustments to AppointmentSlotManager

--- HealthManagementSystem/src/Appointment/AppointmentSlotManager.java
+++ HealthManagementSystem/src/Appointment/AppointmentSlotManager.java
@@ -6,26 +6,22 @@ import java.util.List;
 import FileManager.*;
 
 public class AppointmentSlotManager implements AppointmentSlotService{
-    private List<String[]> appointmentSlotListCsv;
     private DataProcessor dataProcessor;
    
 
-    //sync the appointmentSlotList with CSV file
     public AppointmentSlotManager(dataReader reader, dataWriter writer){
         dataProcessor = new DataProcessor(reader, writer);
-        appointmentSlotListCsv = dataProcessor.readData("AvailabilitySlot_List.csv");
     }
 
     //doctor update availability --> existing appointmentslot / scheduled appointment
     public void setAppointmentAvailability(String appointmentSlotID, String availability){
+        List<String[]> appointmentSlotListCsv = dataProcessor.readData("AvailabilitySlot_List.csv");
         
         for(int i = 0; i<appointmentSlotListCsv.size(); i++){// iterates over each String[] (1 row in csv)
             String[] row = appointmentSlotListCsv.get(i);// get the row 
             if(row[0].equalsIgnoreCase(appointmentSlotID)){ // check if appointmentSlotID matches
                 dataProcessor.writeData("AvailabilitySlot_List.csv", i, 5, availability);
-                System.out.println("Appointment Slot Availability updated");   
-                //update appointmentSlotListCsv array
-                row[5] = availability;         
+                System.out.println("Appointment Slot Availability updated");          
                 
             }
         }
@@ -34,7 +30,8 @@ public class AppointmentSlotManager implements AppointmentSlotService{
     //doctor creates an appointmentSlot --> method assigns an appointmentID
     //assumes doctor is not dumb to create an appointmentSlot again when he has 1 already
     public void setAppointmentAvailability(String doctorID, String doctorName, String appointmentDate, String appointmentTime, String availability){
-       //creating appointmentSlotID with doctorid + date + time
+       
+        //creating appointmentSlotID with doctorid + date + time
         String day = appointmentDate.substring(0,2);
         String[] time = appointmentTime.split(":");
         String hour = time[0].trim();
@@ -51,13 +48,12 @@ public class AppointmentSlotManager implements AppointmentSlotService{
         dataProcessor.writeRow("AvailabilitySlot_List.csv", newRow);
         System.out.println("Appointment Slot Availability updated"); 
         
-        //update appointmentSlotListCsv array
-        String[] newArray = {appointmentSlotID, doctorID, doctorName, appointmentDate, appointmentTime, availability};
-        appointmentSlotListCsv.add(newArray);
     }
     
     //patient view appointmentslots --> updated to only view available slots
     public void viewAvailableAppointmentSlots(){
+        List<String[]> appointmentSlotListCsv = dataProcessor.readData("AvailabilitySlot_List.csv");
+        
         String[] labels = {
             "Appointment Slot ID: ",
             "Doctor ID: ",
