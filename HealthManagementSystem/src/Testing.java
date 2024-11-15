4e80db3ae61928e59b49ad7141e0a9dfef933259
creamrain <leerennie17@gmail.com>
2024-11-15 17:39:35 +0800

    nth much

--- HealthManagementSystem/src/Testing.java
+++ HealthManagementSystem/src/Testing.java
@@ -58,7 +58,7 @@ public class Testing {
         //appointment.scheduleAppointment("P1001", "D100111900");
 
         //check case 5 --> work for if available and booked
-        appointment.rescheduleAppointment("P1001D100111900", "D1002111300");
+        //appointment.rescheduleAppointment("P1001D100111900", "D1002111300");
 
         //check case 6 --> work 
         //appointment.cancelAppointment("P1001D100212600");
